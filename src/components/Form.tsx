81ed7835cf7362c98dd900f7cdb1502de9db6cc1
fabricioFalcao <fabriciohfd@gmail.com>
2023-08-23 17:16:00 -0300

    Requisito 2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,22 @@
 function Form() {
-
+  return (
+    <div>
+      <label htmlFor="service">Nome do serviço</label>
+      <input type="text" id="service" />
+      <br />
+      <label htmlFor="login">Login</label>
+      <input type="text" id="login" />
+      <br />
+      <label htmlFor="password">Senha</label>
+      <input type="password" id="password" />
+      <br />
+      <label htmlFor="url">URL</label>
+      <input type="text" id="url" />
+      <br />
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </div>
+  );
 }
 
 export default Form;
