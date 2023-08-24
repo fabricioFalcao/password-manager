fe89816acd3d9219eb51f7fc14ac9ef3c33fbcc2
fabricioFalcao <fabriciohfd@gmail.com>
2023-08-24 12:50:33 -0300

    Requisito 4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,77 @@
-import { SetShowFormType } from '../types';
+import { useState } from 'react';
+import { FormDataType, SetShowFormType } from '../types';
+
+const INITIAL_STATE = {
+  service: '',
+  login: '',
+  password: '',
+  url: '',
+} as FormDataType;
 
 function Form({ setShowForm }:SetShowFormType) {
+  const [formData, setFormData] = useState(INITIAL_STATE);
+  const { service, login, password, url } = formData;
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+  ) => {
+    const { id, value } = event.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const serviceFilledIn = formData.service.trim().length > 0;
+  const loginFilledIn = formData.login.trim().length > 0;
+  const passwordAbove8 = formData.password.trim().length >= 8;
+  const passwordBelow16 = formData.password.trim().length <= 16;
+  const passwordNumbersAndLetters = /(?=.*[0-9])(?=.*[a-zA-Z])/.test(formData.password);
+  const passwordSpecialCharacter = /[^a-zA-Z0-9_]/.test(formData.password);
+  const validForm = (serviceFilledIn && loginFilledIn && passwordAbove8 && passwordBelow16
+      && passwordNumbersAndLetters && passwordSpecialCharacter);
+
   return (
     <div>
-      <label htmlFor="service">Nome do serviço</label>
-      <input type="text" id="service" />
-      <br />
-      <label htmlFor="login">Login</label>
-      <input type="text" id="login" />
-      <br />
-      <label htmlFor="password">Senha</label>
-      <input type="password" id="password" />
-      <br />
-      <label htmlFor="url">URL</label>
-      <input type="text" id="url" />
-      <br />
-      <button>Cadastrar</button>
-      <button onClick={ () => setShowForm(false) }>Cancelar</button>
+      <form action="">
+        <label htmlFor="service">Nome do serviço</label>
+        <input
+          type="text"
+          id="service"
+          required
+          value={ service }
+          onChange={ handleChange }
+        />
+        <br />
+        <label htmlFor="login">Login</label>
+        <input
+          type="text"
+          id="login"
+          required
+          value={ login }
+          onChange={ handleChange }
+        />
+        <br />
+        <label htmlFor="password">Senha</label>
+        <input
+          type="password"
+          id="password"
+          required
+          value={ password }
+          onChange={ handleChange }
+        />
+        <br />
+        <label htmlFor="url">URL</label>
+        <input
+          type="text"
+          id="url"
+          value={ url }
+          onChange={ handleChange }
+        />
+        <br />
+        <button type="submit" disabled={ !validForm }>Cadastrar</button>
+        <button onClick={ () => setShowForm(false) }>Cancelar</button>
+      </form>
     </div>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
 export type SetShowFormType = {
   setShowForm: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+export type FormDataType = {
+  service: string,
+  login: string,
+  password: string,
+  url: string,
+};
